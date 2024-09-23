e71c1bfd5b6596eff60d23534e6c09fafd252800
Ritik Kumar <ritik.kumar006@gmail.com>
2024-09-23 05:38:56 +0530

    starting off

---
+++ 2010B.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << 6 - (a + b) << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  // cin >> t;
+  t = 1;
+  while(t--) {
+    solve();
+  }
+}
---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define MOD 1e9 + 7
+#define INF 1e18
+#define nline '\n'
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define F first
+#define S second
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+}
